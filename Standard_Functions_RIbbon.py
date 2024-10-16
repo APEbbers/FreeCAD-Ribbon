0d9d746767f444bac068f2603be88342414b2d02
Paul Ebbers <paul.ebbers@gmail.com>
2024-10-17 00:07:36 +0200

    updated getRepoAdress function

--- Standard_Functions_RIbbon.py
+++ Standard_Functions_RIbbon.py
@@ -178,9 +178,7 @@ def restart_freecad():
 
     args = QtWidgets.QApplication.arguments()[1:]
     if Gui.getMainWindow().close():
-        QtCore.QProcess.startDetached(
-            QtWidgets.QApplication.applicationFilePath(), args
-        )
+        QtCore.QProcess.startDetached(QtWidgets.QApplication.applicationFilePath(), args)
 
     return
 
@@ -333,13 +331,9 @@ def GetFileDialog(Filter="", parent=None, DefaultPath="", SaveAs: bool = True) -
 
     file = ""
     if SaveAs is False:
-        file = QFileDialog.getOpenFileName(
-            parent=parent, caption="Select a file", dir=DefaultPath, filter=Filter
-        )[0]
+        file = QFileDialog.getOpenFileName(parent=parent, caption="Select a file", dir=DefaultPath, filter=Filter)[0]
     if SaveAs is True:
-        file = QFileDialog.getSaveFileName(
-            parent=parent, caption="Select a file", dir=DefaultPath, filter=Filter
-        )[0]
+        file = QFileDialog.getSaveFileName(parent=parent, caption="Select a file", dir=DefaultPath, filter=Filter)[0]
     return file
 
 
@@ -347,25 +341,26 @@ def GetFolder(parent=None, DefaultPath="") -> str:
     from PySide.QtWidgets import QFileDialog
 
     Directory = ""
-    Directory = QFileDialog.getExistingDirectory(
-        parent=parent, caption="Select Folder", dir=DefaultPath
-    )
+    Directory = QFileDialog.getExistingDirectory(parent=parent, caption="Select Folder", dir=DefaultPath)
 
     return Directory
 
 
-def getReproAdress(base_path):
+def getRepoAdress(base_path):
     import pathlib
     import os
 
-    if base_path == "":
-        base_path = os.path.dirname(__file__)
+    try:
+        if base_path == "":
+            base_path = os.path.dirname(__file__)
 
-    git_dir = pathlib.Path(base_path) / ".git"
-    with (git_dir / "FETCH_HEAD").open("r") as head:
-        ref = head.readline().split(" ")[-1].strip()
+        git_dir = pathlib.Path(base_path) / ".git"
+        with (git_dir / "FETCH_HEAD").open("r") as head:
+            ref = head.readline().split(" ")[-1].strip()
 
         return ref
+    except Exception:
+        return
 
 
 def CreateToolbar(Name: str, WorkBenchName: str = "Global", ButtonList: list = []):
@@ -374,9 +369,7 @@ def CreateToolbar(Name: str, WorkBenchName: str = "Global", ButtonList: list = [
     # Define the name for the toolbar
     ToolBarName = Name
     # define the parameter path for the toolbar
-    WorkbenchToolBarsParamPath = (
-        "User parameter:BaseApp/Workbench/" + ToolbarGroupName + "/Toolbar/"
-    )
+    WorkbenchToolBarsParamPath = "User parameter:BaseApp/Workbench/" + ToolbarGroupName + "/Toolbar/"
 
     # add the ToolbarGroup in the FreeCAD Parameters
     WorkbenchToolbar = App.ParamGet(WorkbenchToolBarsParamPath + ToolBarName)
@@ -402,9 +395,7 @@ def RemoveWorkBenchToolbars(Name: str, WorkBenchName: str = "Global") -> None:
     # Define the name for the toolbar
     ToolBarName = Name
     # define the parameter path for the toolbar
-    ToolBarsParamPath = (
-        "User parameter:BaseApp/Workbench/" + ToolbarGroupName + "/Toolbar/"
-    )
+    ToolBarsParamPath = "User parameter:BaseApp/Workbench/" + ToolbarGroupName + "/Toolbar/"
 
     custom_toolbars = App.ParamGet(ToolBarsParamPath)
     custom_toolbars.RemGroup(ToolBarName)
