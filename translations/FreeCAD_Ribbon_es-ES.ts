1c9acd8495ed67d2fdee5ea2a11abd3fb92e882f
hasecilu <hasecilu@tuta.io>
2024-10-28 17:35:00 -0600

    Add Spanish translations

---
+++ translations/FreeCAD_Ribbon_es-ES.ts
@@ -0,0 +1,447 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="en_US">
+<context>
+    <name>Design</name>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="44"/>
+        <location filename="../Resources/ui/Design.ui" line="1001"/>
+        <source>Ribbon design</source>
+        <translation>Diseño de cinta</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="71"/>
+        <source>Close</source>
+        <translation>Cerrar</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="88"/>
+        <source>Restore</source>
+        <translation>Restaurar</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="95"/>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="102"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="105"/>
+        <source>Esc</source>
+        <translation>Esc</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="128"/>
+        <source>Reset</source>
+        <translation>Restablecer</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="169"/>
+        <source>Quick access toolbar</source>
+        <translation>Barra de herramientas de acceso rápido</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="293"/>
+        <location filename="../Resources/ui/Design.ui" line="506"/>
+        <source>Category:</source>
+        <translation>Categoría:</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="305"/>
+        <source>Select commands to add to the quick access toolbar</source>
+        <translation>Seleccionar los comandos a agregar a la barra de herramientas de acceso rápido</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="322"/>
+        <location filename="../Resources/ui/Design.ui" line="340"/>
+        <location filename="../Resources/ui/Design.ui" line="406"/>
+        <location filename="../Resources/ui/Design.ui" line="487"/>
+        <location filename="../Resources/ui/Design.ui" line="573"/>
+        <location filename="../Resources/ui/Design.ui" line="594"/>
+        <location filename="../Resources/ui/Design.ui" line="961"/>
+        <location filename="../Resources/ui/Design.ui" line="985"/>
+        <location filename="../Resources/ui/Design.ui" line="1477"/>
+        <source>New Item</source>
+        <translation>Nuevo elemento</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="357"/>
+        <location filename="../Resources/ui/Design.ui" line="524"/>
+        <source>Type to search...</source>
+        <translation>Escriba para buscar...</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="369"/>
+        <source>Exclude panels</source>
+        <translation>Excluir paneles</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="470"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select panels to &lt;span style=&quot; font-weight:600;&quot;&gt;exclude&lt;/span&gt; from the ribbon.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Seleccionar paneles a &lt;span style=&quot; font-weight:600;&quot;&gt;excluir&lt;/span&gt; de la cnta.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="536"/>
+        <source>Include workbenches</source>
+        <translation>Incluir bancos de trabajo</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="655"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select workbenches to&lt;span style=&quot; font-weight:600;&quot;&gt; include&lt;/span&gt; in the ribbon.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Seleccionar bancos de trabajo a&lt;span style=&quot; font-weight:600;&quot;&gt; incluir&lt;/span&gt; en la cinta.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="664"/>
+        <source>Create custom panels</source>
+        <translation>Crear paneles personalizados</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="706"/>
+        <source>Enter the name of your custom panel...</source>
+        <translation>Ingrese el nombre de su panel personalizado...</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="719"/>
+        <source>Panel name</source>
+        <translation>Nombre del panel</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="732"/>
+        <location filename="../Resources/ui/Design.ui" line="1048"/>
+        <source>Select workbench:</source>
+        <translation>Seleccionar banco de trabajo:</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="757"/>
+        <source>Add</source>
+        <translation>Agregar</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="802"/>
+        <source>select custom panel: </source>
+        <translation>Seleccionar panel personalizado:</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="821"/>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="871"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select panels to add to the custom panel.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Seleccionar paneles para agregar al panel personalizado.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1028"/>
+        <source>Select panel:</source>
+        <translation>Seleccionar panel:</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1055"/>
+        <source>Icon only</source>
+        <translation>Sólo icono</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1080"/>
+        <source> Set the icon size</source>
+        <translation>Establecer el tamaño de icono</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1200"/>
+        <source>Command</source>
+        <translation>Comando</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1205"/>
+        <source>Small</source>
+        <translation>Pequeño</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1210"/>
+        <source>Medium</source>
+        <translation>Mediano</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1215"/>
+        <source>Large</source>
+        <translation>Grande</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1220"/>
+        <source>Command 1</source>
+        <translation>Comando 1</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1309"/>
+        <source>Add separator</source>
+        <translation>Agregar separador</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1328"/>
+        <source>Remove separator</source>
+        <translation>Eliminar separador</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Design.ui" line="1374"/>
+        <source> Set the panel order</source>
+        <translation>Establecer el orden del panel</translation>
+    </message>
+</context>
+<context>
+    <name>FreeCAD Ribbon</name>
+    <message>
+        <location filename="../FCBinding.py" line="160"/>
+        <source>FreeCAD Ribbon: </source>
+        <translation>FreeCAD Ribbon: </translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="472"/>
+        <source>Go to the FreeCAD help page</source>
+        <translation>Ir a la página de ayuda de FreeCAD</translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="485"/>
+        <source>Pin Ribbon</source>
+        <translation>Anclar cinta</translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="490"/>
+        <source>Click to toggle the autohide function on or off</source>
+        <translation>Clic para alternar la función de ocultado automático</translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="512"/>
+        <source>FreeCAD Ribbon</source>
+        <translation>FreeCAD Ribbon</translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="527"/>
+        <source>Customize...</source>
+        <translation>Personalizar...</translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="530"/>
+        <source>Ribbon Design</source>
+        <translation>Diseño de cinta</translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="535"/>
+        <source>Ribbon Preferences</source>
+        <translation>Preferencias de Ribbon</translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="544"/>
+        <source>Scripts</source>
+        <translation>Scripts</translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="557"/>
+        <source>About FreeCAD Ribbon </source>
+        <translation>Acerca de FreeCAD RIbbon </translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="568"/>
+        <source>All workbenches need to be loaded.
+This can take a couple of minutes.
+Do you want to proceed?</source>
+        <translation>Todos los bancos de trabajo tienen que ser cargados.
+Esto puede tomar varios minutos.
+¿Desea proceder?</translation>
+    </message>
+    <message>
+        <location filename="../FCBinding.py" line="1021"/>
+        <source>Given button size not implemented, only small, medium and large are available.</source>
+        <translation>El tamaño de botón dado no está implementado, solo pequeño, mediano y grande están disponibles.</translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="36"/>
+        <source>Activating Ribbon Bar...</source>
+        <translation>Activando barra de Ribbon...</translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="115"/>
+        <source>Ribbon design</source>
+        <translation>Diseño de cinta</translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="277"/>
+        <source>New</source>
+        <translation>Nuevo</translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="892"/>
+        <source>Enter a name for your custom panel first!</source>
+        <translation>¡Primero ingrese un nombre para su panel personalizado!</translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="1777"/>
+        <source>Select a backup file</source>
+        <translation>Seleccione un archovo de copia de seguridad</translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="1788"/>
+        <source>Ribbon bar set back to settings from: </source>
+        <translation>Barra Ribbon establecida a los ajustes de: </translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="1826"/>
+        <source>Ribbon bar reset from </source>
+        <translation>Barra Ribbon restablecida desde </translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="1794"/>
+        <source>Settings reset to </source>
+        <translation>Ajustes restablecidos a </translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="1800"/>
+        <location filename="../LoadDesign_Ribbon.py" line="1822"/>
+        <source>You must restart FreeCAD for changes to take effect.</source>
+        <translation>Debe de reiniciar FreeCAD para que los cambios tomen efecto.</translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="1818"/>
+        <source>Settings reset to default!</source>
+        <translation>¡Ajustes restablecidos a los de por defecto!</translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="1884"/>
+        <source>All</source>
+        <translation>Todo</translation>
+    </message>
+    <message>
+        <location filename="../LoadDesign_Ribbon.py" line="2035"/>
+        <source>Create sketch</source>
+        <translation>Crear croquis</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="17"/>
+        <source>FreeCAD Ribbon settings</source>
+        <translation>Ajustes de FreeCAD RIbbon</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="41"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="44"/>
+        <source>Esc</source>
+        <translation>Esc</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="51"/>
+        <source>Close</source>
+        <translation>Cerrar</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="82"/>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="114"/>
+        <source>Debug mode</source>
+        <translation>Modo de depuración</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="139"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;Debug mode enables extra reports in the report view for debugging purposes.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;El modo de depuración habilita informes extra en la vista de informe para propósitos de depurado.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="162"/>
+        <source>Ribbon settings</source>
+        <translation>Ajustes de cinta</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="175"/>
+        <source>Show text</source>
+        <translation>Mostrar texto</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="188"/>
+        <source>Small buttons</source>
+        <translation>Botones pequeños</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="200"/>
+        <source>Medium buttons</source>
+        <translation>Botones medianos</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="212"/>
+        <source>Large buttons</source>
+        <translation>Botones grandes</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="229"/>
+        <source>Button size</source>
+        <translation>Tamaño de botones</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="254"/>
+        <source>Size of medium buttons:</source>
+        <translation>Tamaño de botones medianos</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="278"/>
+        <source>Size of small buttons:</source>
+        <translation>Tamaño de botones pequeños</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="407"/>
+        <source>Panels</source>
+        <translation>Paneles</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="440"/>
+        <source>No. of columns per panel</source>
+        <translation>Número de columnas por panel</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="466"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;Set to &apos;0&apos;  to disable the maximum of columns&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;Establecer a &apos;0&apos;  para deshabilitar el máximo de columnas&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="497"/>
+        <source>Select stylesheet</source>
+        <translation>Seleccionar hoja de estilo</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="509"/>
+        <location filename="../Resources/ui/Settings.ui" line="617"/>
+        <source>...\</source>
+        <translation>...\</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="534"/>
+        <location filename="../Resources/ui/Settings.ui" line="642"/>
+        <source>Browse..</source>
+        <translation>Buscar...</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="558"/>
+        <source>Backup settings</source>
+        <translation>Ajustes de copia de seguridad</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="581"/>
+        <source>Create backup</source>
+        <translation>Crear copia de seguridad</translation>
+    </message>
+    <message>
+        <location filename="../Resources/ui/Settings.ui" line="608"/>
+        <source>Backup location</source>
+        <translation>Ubicación de copia de seguridad</translation>
+    </message>
+</context>
+</TS>
