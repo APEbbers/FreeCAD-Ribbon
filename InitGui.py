3a90847a3ac63ea7d7d4b8868aaec8a7a5f67f7e
Paul Ebbers <paul.ebbers@gmail.com>
2024-11-29 22:07:20 +0100

    Added comments

--- InitGui.py
+++ InitGui.py
@@ -60,6 +60,7 @@ Gui.removeWorkbench("TestWorkbench")
 # preferences = App.ParamGet("User parameter:BaseApp/Preferences/DockWindows")
 # preferences.SetBool("ActivateOverlay", False)
 
+# make sure that the ribbon will be shown on startup -> reset OverlayTop
 preferences = App.ParamGet("User parameter:BaseApp/MainWindow/DockWindows/OverlayTop")
 preferences.SetString("Widgets", "")
 
