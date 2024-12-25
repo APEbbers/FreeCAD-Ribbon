956326c26ab32c08e8e54001c0eeaaf7d249f14d
Paul Ebbers <paul.ebbers@gmail.com>
2024-12-25 15:25:36 +0100

    Test with overlay + fix in PyQtRibbon package

--- InitGui.py
+++ InitGui.py
@@ -37,13 +37,9 @@ translate = App.Qt.translate
 
 # check if there is a "RibbonStructure.json". if not create one
 file = os.path.join(os.path.dirname(FCBinding.__file__), "RibbonStructure.json")
-file_default = os.path.join(
-    os.path.dirname(FCBinding.__file__), "RibbonStructure_default.json"
-)
+file_default = os.path.join(os.path.dirname(FCBinding.__file__), "RibbonStructure_default.json")
 source = os.path.join(os.path.dirname(FCBinding.__file__), "CreateStructure.txt")
-source_default = os.path.join(
-    os.path.dirname(FCBinding.__file__), "CreateStructure.txt"
-)
+source_default = os.path.join(os.path.dirname(FCBinding.__file__), "CreateStructure.txt")
 
 # check if file exits
 fileExists = os.path.isfile(file)
@@ -60,13 +56,13 @@ if fileExists is False:
 # remove the test workbench
 Gui.removeWorkbench("TestWorkbench")
 
-# Disable the overlay function
-preferences = App.ParamGet("User parameter:BaseApp/Preferences/DockWindows")
-preferences.SetBool("ActivateOverlay", False)
+# # Disable the overlay function
+# preferences = App.ParamGet("User parameter:BaseApp/Preferences/DockWindows")
+# preferences.SetBool("ActivateOverlay", False)
 
-# make sure that the ribbon will be shown on startup -> reset OverlayTop
-preferences = App.ParamGet("User parameter:BaseApp/MainWindow/DockWindows/OverlayTop")
-preferences.SetString("Widgets", "")
+# # make sure that the ribbon will be shown on startup -> reset OverlayTop
+# preferences = App.ParamGet("User parameter:BaseApp/MainWindow/DockWindows/OverlayTop")
+# preferences.SetString("Widgets", "")
 
 try:
     print(translate("FreeCAD Ribbon", "Activating Ribbon Bar..."))
