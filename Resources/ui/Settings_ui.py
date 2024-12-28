35efe54daaef2f14f695696e131c6df4cb762acc
Paul Ebbers <paul.ebbers@gmail.com>
2024-12-28 17:07:40 +0100

    Custom colors - 2

--- Resources/ui/Settings_ui.py
+++ Resources/ui/Settings_ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'SettingsKHiHtM.ui'
+## Form generated from reading UI file 'SettingsKxonhT.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.2
 ##
