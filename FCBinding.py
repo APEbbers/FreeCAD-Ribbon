7a8d6d3e71e7468af7a9e920e14957d436297084
Paul Ebbers <paul.ebbers@gmail.com>
2025-01-18 20:43:36 +0100

    test with menus on macOs - 8

--- FCBinding.py
+++ FCBinding.py
@@ -991,17 +991,15 @@ class ModernMenu(RibbonBar):
         AboutButton.triggered.connect(self.on_AboutButton_clicked)
 
         if platform.system() == "darwin":
+
             if Parameters_Ribbon.USE_FC_OVERLAY is False:
-                MenuBar.addSeparator()
                 MenuBar.addMenu(OverlayMenu)
             # Add the ribbon design button
-            MenuBar.addSeparator()
-            MenuBar.addMenu(DesignMenu).setMenuRole(QAction.MenuRole.PreferencesRole)
-            MenuBar.addSeparator()
-            MenuBar.addMenu(WhatsNewButton).setMenuRole(QAction.MenuRole.PreferencesRole)
+            MenuBar.addMenu(DesignMenu)
+            MenuBar.addMenu(WhatsNewButton)
             # MenuBar.addMenu(RibbonHelpButton).setMenuRole(QAction.MenuRole.ApplicationSpecificRole)
             # MenuBar.addMenu(AboutButton).setMenuRole(QAction.MenuRole.ApplicationSpecificRole)
-            mw.setMenuBar(MenuBar)
+
         return
 
     def loadDesignMenu(self):
