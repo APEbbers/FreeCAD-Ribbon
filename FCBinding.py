0cf617996a54aa11ef74d1ec85467c2b6c5669b3
Paul Ebbers <paul.ebbers@gmail.com>
2025-02-07 19:36:30 +0100

    Tweaked fontsizes - 3

--- FCBinding.py
+++ FCBinding.py
@@ -23,7 +23,7 @@ import FreeCAD as App
 import FreeCADGui as Gui
 from pathlib import Path
 
-from PySide6.QtGui import (
+from PySide.QtGui import (
     QIcon,
     QAction,
     QPixmap,
@@ -41,7 +41,7 @@ from PySide6.QtGui import (
     QKeySequence,
     QShortcut,
 )
-from PySide6.QtWidgets import (
+from PySide.QtWidgets import (
     QToolButton,
     QToolBar,
     QSizePolicy,
@@ -67,7 +67,7 @@ from PySide6.QtWidgets import (
     QToolTip,
     QWidgetItem,
 )
-from PySide6.QtCore import (
+from PySide.QtCore import (
     Qt,
     QTimer,
     Signal,
