74f57155774cd2c6fca09069d59ee56dbb816c05
Paul Ebbers <paul.ebbers@gmail.com>
2025-02-10 20:13:49 +0100

    Added try-catch statement for changeColor

--- Resources/packages/pyqtribbon_local/tabbar.py
+++ Resources/packages/pyqtribbon_local/tabbar.py
@@ -54,9 +54,7 @@ class RibbonTabBar(QTabBar):
         self._tabColors[text] = color
         return super().addTab(text)
 
-    def addAssociatedTabs(
-        self, name: str, texts: typing.List[str], color: QColor
-    ) -> typing.List[int]:
+    def addAssociatedTabs(self, name: str, texts: typing.List[str], color: QColor) -> typing.List[int]:
         """Add associated multiple tabs which have the same color to the tab bar.
 
         :param name: The name of the context category.
@@ -93,12 +91,12 @@ class RibbonTabBar(QTabBar):
         """Change tab's color."""
 
         if self.count() > 0:
-            currentTabText = self.tabText(inx)
-            currentTabColor = self._tabColors[currentTabText]
-            if currentTabColor is not None:
-                self.setStyleSheet(
-                    "RibbonTabBar::tab:selected {color: %s;}"
-                    % QColor(currentTabColor).name()
-                )
-            else:
-                self.setStyleSheet("RibbonTabBar::tab:selected {color: black;}")
+            try:
+                currentTabText = self.tabText(inx)
+                currentTabColor = self._tabColors[currentTabText]
+                if currentTabColor is not None:
+                    self.setStyleSheet("RibbonTabBar::tab:selected {color: %s;}" % QColor(currentTabColor).name())
+                else:
+                    self.setStyleSheet("RibbonTabBar::tab:selected {color: black;}")
+            except Exception as e:
+                print(e)
