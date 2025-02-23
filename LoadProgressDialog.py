831adce8648c992ddfb3cddf4fdd3660d63c140e
Paul Ebbers <paul.ebbers@gmail.com>
2025-02-23 09:51:52 +0100

    Added a progressdialog  for testing

---
+++ LoadProgressDialog.py
@@ -0,0 +1,104 @@
+# *************************************************************************
+# *                                                                       *
+# * Copyright (c) 2019-2024 Paul Ebbers                                   *
+# *                                                                       *
+# * This program is free software; you can redistribute it and/or modify  *
+# * it under the terms of the GNU Lesser General Public License (LGPL)    *
+# * as published by the Free Software Foundation; either version 3 of     *
+# * the License, or (at your option) any later version.                   *
+# * for detail see the LICENCE text file.                                 *
+# *                                                                       *
+# * This program is distributed in the hope that it will be useful,       *
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# * GNU Library General Public License for more details.                  *
+# *                                                                       *
+# * You should have received a copy of the GNU Library General Public     *
+# * License along with this program; if not, write to the Free Software   *
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# * USA                                                                   *
+# *                                                                       *
+# *************************************************************************
+import FreeCAD as App
+import FreeCADGui as Gui
+import os
+
+from PySide.QtCore import Qt, SIGNAL
+from PySide.QtWidgets import (
+    QTabWidget,
+    QSlider,
+    QSpinBox,
+    QCheckBox,
+    QComboBox,
+    QLabel,
+    QDialogButtonBox,
+    QApplication,
+    QPushButton,
+    QDialog,
+)
+from PySide.QtGui import QIcon, QPixmap
+import sys
+
+import Standard_Functions_RIbbon as StandardFunctions
+import Parameters_Ribbon
+
+# Get the resources
+pathIcons = Parameters_Ribbon.ICON_LOCATION
+pathStylSheets = Parameters_Ribbon.STYLESHEET_LOCATION
+pathUI = Parameters_Ribbon.UI_LOCATION
+pathBackup = Parameters_Ribbon.BACKUP_LOCATION
+sys.path.append(pathIcons)
+sys.path.append(pathStylSheets)
+sys.path.append(pathUI)
+sys.path.append(pathBackup)
+
+# import graphical created Ui. (With QtDesigner or QtCreator)
+import ui_ProgressDialog as ui_ProgressDialog
+
+# Define the translation
+translate = App.Qt.translate
+
+
+class LoadDialog(ui_ProgressDialog.Ui_Dialog):
+
+    Text = ""
+
+    def __init__(self, Text):
+        # Makes "self.on_CreateBOM_clicked" listen to the changed control values instead initial values
+        super(LoadDialog, self).__init__()
+
+        # # this will create a Qt widget from our ui file
+        self.form = Gui.PySideUic.loadUi(os.path.join(pathUI, "ProgressDialog.ui"))
+
+        # Make sure that the dialog stays on top
+        # self.form.setWindowFlags(Qt.WindowType.WindowStaysOnTopHint)
+        self.form.setWindowFlags(Qt.WindowType.Tool)
+        self.form.setWindowModality(Qt.WindowModality.WindowModal)
+
+        # Get the style from the main window and use it for this form
+        mw = Gui.getMainWindow()
+        palette = mw.palette()
+        self.form.setPalette(palette)
+        Style = mw.style()
+        self.form.setStyle(Style)
+
+        # Set the text
+        self.form.label.setText(Text)
+
+        return
+
+    @classmethod
+    def update(self, value):
+        self.Text += value
+
+        return
+
+
+def main(Text):
+    # Get the form
+    Dialog = LoadDialog(Text).form
+
+    # Show the form
+    Dialog.show()
+
+    return
