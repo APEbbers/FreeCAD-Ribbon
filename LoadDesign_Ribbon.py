227b8ce793267ab53f615d3be97c076057da39df
Paul Ebbers <paul.ebbers@gmail.com>
2025-05-10 09:35:07 +0200

    Fixed PySide6 import

--- LoadDesign_Ribbon.py
+++ LoadDesign_Ribbon.py
@@ -22,8 +22,8 @@
 import FreeCAD as App
 import FreeCADGui as Gui
 import os
-from PySide6.QtGui import QIcon, QPixmap, QAction, QGuiApplication
-from PySide6.QtWidgets import (
+from PySide.QtGui import QIcon, QPixmap, QAction, QGuiApplication
+from PySide.QtWidgets import (
     QListWidgetItem,
     QTableWidgetItem,
     QListWidget,
@@ -39,7 +39,7 @@ from PySide6.QtWidgets import (
     QRadioButton,
     QLabel,
 )
-from PySide6.QtCore import Qt, SIGNAL, Signal, QObject, QThread, QSize, QEvent
+from PySide.QtCore import Qt, SIGNAL, Signal, QObject, QThread, QSize, QEvent
 import sys
 import json
 from datetime import datetime
