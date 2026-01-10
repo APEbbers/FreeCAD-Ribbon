6ff42fe9eb84c1977d4ce345bb3afffdf5f3c689
APEbbers <paul.ebbers@gmail.com>
2026-01-10 18:03:39 +0100

    Added commands for creating new panels and adding commands in context menu

--- LoadAddCommands.py
+++ LoadAddCommands.py
@@ -53,61 +53,6 @@ sys.path.append(pathUI)
 sys.path.append(pathBackup)
 
 # import graphical created Ui. (With QtDesigner or QtCreator)
-import # *************************************************************************
-# *                                                                       *
-# * Copyright (c) 2019-2024 Paul Ebbers                                   *
-# *                                                                       *
-# * This program is free software; you can redistribute it and/or modify  *
-# * it under the terms of the GNU Lesser General Public License (LGPL)    *
-# * as published by the Free Software Foundation; either version 3 of     *
-# * the License, or (at your option) any later version.                   *
-# * for detail see the LICENCE text file.                                 *
-# *                                                                       *
-# * This program is distributed in the hope that it will be useful,       *
-# * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# * GNU Library General Public License for more details.                  *
-# *                                                                       *
-# * You should have received a copy of the GNU Library General Public     *
-# * License along with this program; if not, write to the Free Software   *
-# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-# * USA                                                                   *
-# *                                                                       *
-# *************************************************************************
-import FreeCAD as App
-import FreeCADGui as Gui
-import os
-
-from PySide.QtCore import Qt, SIGNAL
-from PySide.QtWidgets import (
-    QTabWidget,
-    QSlider,
-    QSpinBox,
-    QCheckBox,
-    QComboBox,
-    QLabel,
-    QDialogButtonBox,
-    QApplication,
-    QPushButton,
-    QDialog,
-)
-from PySide.QtGui import QIcon, QPixmap
-import sys
-
-import Standard_Functions_Ribbon as StandardFunctions
-import Parameters_Ribbon
-
-# Get the resources
-pathIcons = Parameters_Ribbon.ICON_LOCATION
-pathStylSheets = Parameters_Ribbon.STYLESHEET_LOCATION
-pathUI = Parameters_Ribbon.UI_LOCATION
-pathBackup = Parameters_Ribbon.BACKUP_LOCATION
-sys.path.append(pathIcons)
-sys.path.append(pathStylSheets)
-sys.path.append(pathUI)
-sys.path.append(pathBackup)
-
-# import graphical created Ui. (With QtDesigner or QtCreator)
 import AddCommands_ui as AddCommands_ui
 
 # Define the translation
