37c160ecd3184a8a0a87de2ab35a18aa6f7573a4
APEbbers <paul.ebbers@gmail.com>
2026-04-30 00:10:58 +0200

    Move Form buttons for docked dialog - 2

--- Resources/ui/AddCommands_ui.py
+++ Resources/ui/AddCommands_ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'AddCommandsVnNXRw.ui'
+## Form generated from reading UI file 'AddCommandsqqttae.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.11.0
 ##
