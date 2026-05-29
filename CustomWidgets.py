6ded7ecaa9b1be5cab1762a685439fad33237335
APEbbers <paul.ebbers@gmail.com>
2026-05-29 19:09:44 +0200

    PySide6 to PySide

--- CustomWidgets.py
+++ CustomWidgets.py
@@ -24,7 +24,7 @@ import FreeCADGui as Gui
 import typing
 import sys
 
-from PySide6.QtGui import (
+from PySide.QtGui import (
     QIcon,
     QAction,
     QFontMetrics,
@@ -39,7 +39,7 @@ from PySide6.QtGui import (
     QPainter,
     QEnterEvent,   
 )
-from PySide6.QtWidgets import (
+from PySide.QtWidgets import (
     QComboBox,
     QMainWindow,
     QSizePolicy,
@@ -59,7 +59,7 @@ from PySide6.QtWidgets import (
     QPushButton,
     QGraphicsEffect,
 )
-from PySide6.QtCore import (
+from PySide.QtCore import (
     Qt,
     QSize,
     QMimeData,
