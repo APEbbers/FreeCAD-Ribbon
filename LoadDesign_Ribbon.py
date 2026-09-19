0a140ed1ab5e66bc134cca41203fe58d91f5063e
APEbbers <paul.ebbers@gmail.com>
2026-09-19 21:35:28 +0200

    Bugfixes for legacy layout menu

--- LoadDesign_Ribbon.py
+++ LoadDesign_Ribbon.py
@@ -91,6 +91,8 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
 
     # Set the data file version. Triggeres an question if an update is needed
     DataFileVersion = "1.3"
+    
+    ribbonStructure = {}
 
     # Define list of the workbenches, toolbars and commands on class level
     List_Workbenches = []
@@ -128,10 +130,12 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
     listWidgetItems = []
     listWidgetItems_DDB = []
 
-    def __init__(self, IconList):
+    def __init__(self, IconList, ribbonStructure):
 
         # Makes "self.on_CreateBOM_clicked" listen to the changed control values instead initial values
         super(LoadDialog, self).__init__()
+        
+        self.Dict_RibbonCommandPanel = ribbonStructure
         
         # Load the icons for the commands
         self.List_CommandIcons = IconList
@@ -3136,7 +3140,7 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
         )[0]
 
         # Get the toolbar name
-        Toolbar = self.form.PanelList_RD.currentData(Qt.ItemDataRole.UserRole)
+        Toolbar = self.form.PanelOrder_RD.selectedItems()[0].data(Qt.ItemDataRole.UserRole)
 
         # Define a table widget item
         CommandTable_RDItem = QTableWidgetItem()
@@ -4107,15 +4111,17 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
 
     def ReadJson(self, Section="All", JsonFile=""):
         # Open the JsonFile and load the data
+        data = {}
         try:
             if JsonFile != "":
                 JsonFile = open(JsonFile)
+                data = json.load(JsonFile)
             else:
-                JsonFile = open(Parameters.RIBBON_STRUCTURE_JSON)
+                data = self.Dict_RibbonCommandPanel
         except Exception:
             JsonFile = open(Parameters.RIBBON_STRUCTURE_JSON)
-        data = json.load(JsonFile)
-
+            data = json.load(JsonFile)
+                
         # Get all the ignored toolbars
         if Section == "ignoredToolbars" or Section == "All":
             for IgnoredToolbar in data["ignoredToolbars"]:
@@ -4165,7 +4171,7 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
             except Exception:
                 pass
 
-        JsonFile.close()
+        # JsonFile.close()
         return
 
     def WriteJson(self):
@@ -4206,21 +4212,15 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
             IgnoredWorkbench = ListWidgetItem.data(Qt.ItemDataRole.UserRole)
             List_IgnoredWorkbenches.append(IgnoredWorkbench[2])
 
-        # Create a resulting dict
-        resultingDict = {}
         # add the various lists to the resulting dict.
-        resultingDict["language"] = FCLanguage
-        resultingDict["ignoredToolbars"] = List_IgnoredToolbars
-        resultingDict["iconOnlyToolbars"] = List_IconOnly_Toolbars
-        resultingDict["quickAccessCommands"] = List_QuickAccessCommands
-        resultingDict["ignoredWorkbenches"] = List_IgnoredWorkbenches
-        resultingDict.update(self.Dict_CustomToolbars)
-        resultingDict.update(self.Dict_RibbonCommandPanel)
-        resultingDict.update(self.Dict_NewPanels)
-
-        # RibbonTabs
-        # Get the Ribbon dictionary
-        resultingDict.update(self.Dict_RibbonCommandPanel)
+        self.Dict_RibbonCommandPanel["language"] = FCLanguage
+        self.Dict_RibbonCommandPanel["ignoredToolbars"] = List_IgnoredToolbars
+        self.Dict_RibbonCommandPanel["iconOnlyToolbars"] = List_IconOnly_Toolbars
+        self.Dict_RibbonCommandPanel["quickAccessCommands"] = List_QuickAccessCommands
+        self.Dict_RibbonCommandPanel["ignoredWorkbenches"] = List_IgnoredWorkbenches
+        self.Dict_RibbonCommandPanel.update(self.Dict_CustomToolbars)
+        self.Dict_RibbonCommandPanel.update(self.Dict_RibbonCommandPanel)
+        self.Dict_RibbonCommandPanel.update(self.Dict_NewPanels)
 
         # get the path for the Json file
         JsonFile = Parameters.RIBBON_STRUCTURE_JSON
@@ -4236,7 +4236,7 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
 
         # Writing to sample.json
         with open(JsonFile, "w") as outfile:
-            json.dump(resultingDict, outfile, indent=4)
+            json.dump(self.Dict_RibbonCommandPanel, outfile, indent=4)
 
         outfile.close()
         return
@@ -4298,6 +4298,8 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
                         ) == DestinationItem.data(Qt.ItemDataRole.UserRole):
                             SourceWidget.takeItem(i)
 
+        # Update the data with the (text)changed
+        self.UpdateData()
         return
 
     def MoveItem(self, ListWidget: QListWidget, Up: bool = True):
@@ -4338,6 +4340,7 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
                     CommandTable.setCurrentCell(row - 1, column)
                 CommandTable.removeRow(row + 1)
 
+        # Update the data with the (text)changed
         self.UpdateData()
         return
 
@@ -4355,7 +4358,7 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
         )[0]
 
         # Get the toolbar name
-        Toolbar = self.form.PanelList_RD.currentData(Qt.ItemDataRole.UserRole)
+        Toolbar = self.form.PanelOrder_RD.selectedItems()[0].data(Qt.ItemDataRole.UserRole)
 
         # Define the order based on the order in this table widget
         Order = []
@@ -4372,6 +4375,9 @@ class LoadDialog(Design_ui.Ui_Form, QObject):
         self.Dict_RibbonCommandPanel["workbenches"][WorkBenchName]["toolbars"][Toolbar][
             "order"
         ] = Order
+        
+        # Update the data with the (text)changed
+        self.UpdateData()
 
         return
 
