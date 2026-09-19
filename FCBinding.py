b5edad9cc980c47bf8463a74662404ee4ab25395
APEbbers <paul.ebbers@gmail.com>
2026-09-19 20:08:30 +0200

    Fixed issue with the restore function not being applied after restart

--- FCBinding.py
+++ FCBinding.py
@@ -7620,6 +7620,11 @@ class ModernMenu(RibbonBar):
                 ).format(result),
                 "Warning",
             )
+            
+            # update the ribbon structure from the backup file
+            with open(BackupFile, "r") as file:
+                self.ribbonStructure.update(json.load(file))
+            file.close()
 
             message = translate(
                 "FreeCAD Ribbon",
